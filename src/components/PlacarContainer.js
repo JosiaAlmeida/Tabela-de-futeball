b563809243f0930607efd32511ef1512c4248390
Josia Almeida <josiaalmeida0001@gmail.com>
2021-05-03 12:35:54 +0100

    criando componentes

---
+++ src/components/PlacarContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class PlacarContainer extends Component{
+    render(){
+        return(
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
+}
